conceptualspace/yet-another-speed-dial

commit 150c31610da0ec51a02d36c4607becb8f72c8400

fix for #21

src/js/index.js
@@ -545,15 +545,19 @@ function saveBookmarkSettings() {
 
     // find image index
     if (title !== targetTileTitle) {
+        // allow ui to respond immediately while bookmark updated
         targetNode.children[0].children[1].textContent = title;
         // sortable ids changed so rewrite to storage
         //let order = sortable.toArray();
         //browser.storage.local.set({"sort":order});
+        // todo: temp hack to match all until we start using bookmark ids
         browser.bookmarks.search({url})
-            .then(bookmark => {
-                browser.bookmarks.update(bookmark[0].id, {
-                    title
-                });
+            .then(bookmarks => {
+                for (let bookmark of bookmarks) {
+                    browser.bookmarks.update(bookmark.id, {
+                        title
+                    });
+                }
             })
     }
 

commit 821e3b5a0d178d8086a3d43972a0b1418f4aee4c

title editing fix
- only update the title for the currently edited bookmark (if another bookmark has the same url in a different folder it is treated separately). fixes a bug introduced by 150c316 where the multiple updates created duplicate elements in the ui

src/js/index.js
@@ -581,10 +581,13 @@ function saveBookmarkSettings() {
                     browser.storage.local.remove(url)
                 }
                 for (let bookmark of bookmarks) {
-                    browser.bookmarks.update(bookmark.id, {
-                        title,
-                        url: newUrl
-                    });
+                    let currentParent = currentFolder ? currentFolder : speedDialId
+                    if (bookmark.parentId === currentParent) {
+                        browser.bookmarks.update(bookmark.id, {
+                            title,
+                            url: newUrl
+                        });
+                    }
                 }
             })
     }