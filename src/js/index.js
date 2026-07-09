8e31f777dd2408a66836a7b1d302ca04ca079cb5
conceptualspace <dev@conceptualspace.net>
2026-07-09 03:00:36 +0200

    update welcome splash
    - stylin
    - update some missing translations

--- src/js/index.js
+++ src/js/index.js
@@ -696,25 +696,45 @@ async function printNewSetup() {
         bookmarksContainerParent.append(folderContainerEl);
     }
 
+    const featureIcons = [
+        // add bookmarks (star)
+        '<svg class="welcome-feature-icon" xmlns="http://www.w3.org/2000/svg" height="22" viewBox="0 -960 960 960" width="22" fill="currentColor"><path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"/></svg>',
+        // organize into folders
+        '<svg class="welcome-feature-icon" xmlns="http://www.w3.org/2000/svg" height="22" viewBox="0 -960 960 960" width="22" fill="currentColor"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640v400q0 33-23.5 56.5T800-160H160Zm0-80h640v-400H447l-80-80H160v480Zm0 0v-480 480Z"/></svg>',
+        // edit / rearrange
+        '<svg class="welcome-feature-icon" xmlns="http://www.w3.org/2000/svg" height="22" viewBox="0 -960 960 960" width="22" fill="currentColor"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>'
+    ];
+
     const noBookmarksDiv = document.createElement('div');
     noBookmarksDiv.className = 'default-content';
     noBookmarksDiv.id = 'noBookmarks';
     noBookmarksDiv.innerHTML = `
-        <h1 class="default-content" data-locale="newInstall1">${chrome.i18n.getMessage('newInstall1')}</h1>
-        <p class="default-content helpText" data-locale="newInstall2">${chrome.i18n.getMessage('newInstall2')}</p>
-        <p class="default-content helpText" data-locale="newInstall3">${chrome.i18n.getMessage('newInstall3')}</p>
-        <p class="default-content helpText" data-locale="newInstall4">${chrome.i18n.getMessage('newInstall4')}</p>
-        <div class="cta-container">
-        <p id="splashImport" class="default-content helpText cta" >
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M260-160q-91 0-155.5-63T40-377q0-78 47-139t123-78q25-92 100-149t170-57q117 0 198.5 81.5T760-520q69 8 114.5 59.5T920-340q0 75-52.5 127.5T740-160H520q-33 0-56.5-23.5T440-240v-206l-64 62-56-56 160-160 160 160-56 56-64-62v206h220q42 0 71-29t29-71q0-42-29-71t-71-29h-60v-80q0-83-58.5-141.5T480-720q-83 0-141.5 58.5T280-520h-20q-58 0-99 41t-41 99q0 58 41 99t99 41h100v80H260Zm220-280Z"/></svg>
-            Import
-        </p>
-        <p id="splashAddDial" class="default-content helpText cta" >
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
-        Add Site
-        </p>
+        <div class="welcome-card">
+            <svg class="welcome-logo" viewBox="0 0 128 128" width="72" height="72" xmlns="http://www.w3.org/2000/svg" focusable="false" aria-hidden="true">
+                <rect class="welcome-logo-tile" style="--i:0" x="5" y="6" width="40" height="40" rx="3"/>
+                <rect class="welcome-logo-tile" style="--i:1" x="83" y="6" width="40" height="40" rx="3"/>
+                <rect class="welcome-logo-tile" style="--i:2" x="44" y="44" width="40" height="40" rx="3"/>
+                <rect class="welcome-logo-tile" style="--i:3" x="5" y="82" width="40" height="40" rx="3"/>
+            </svg>
+            <h1 class="welcome-title"><span>${chrome.i18n.getMessage('newInstallTitleLine1')}</span><span>${chrome.i18n.getMessage('newInstallTitleLine2')}</span></h1>
+            <ul class="welcome-features">
+                <li class="welcome-feature">${featureIcons[0]}<span>${chrome.i18n.getMessage('newInstall2')}</span></li>
+                <li class="welcome-feature">${featureIcons[1]}<span>${chrome.i18n.getMessage('newInstall3')}</span></li>
+                <li class="welcome-feature">${featureIcons[2]}<span>${chrome.i18n.getMessage('newInstall4')}</span></li>
+            </ul>
+            <div class="cta-container">
+                <button id="splashAddDial" class="welcome-cta welcome-cta--primary" type="button">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="22" viewBox="0 -960 960 960" width="22" fill="currentColor"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
+                    <span>${chrome.i18n.getMessage('newInstallAddSite')}</span>
+                </button>
+                <button id="splashImport" class="welcome-cta welcome-cta--secondary" type="button">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="22" viewBox="0 -960 960 960" width="22" fill="currentColor"><path d="M260-160q-91 0-155.5-63T40-377q0-78 47-139t123-78q25-92 100-149t170-57q117 0 198.5 81.5T760-520q69 8 114.5 59.5T920-340q0 75-52.5 127.5T740-160H520q-33 0-56.5-23.5T440-240v-206l-64 62-56-56 160-160 160 160-56 56-64-62v206h220q42 0 71-29t29-71q0-42-29-71t-71-29h-60v-80q0-83-58.5-141.5T480-720q-83 0-141.5 58.5T280-520h-20q-58 0-99 41t-41 99q0 58 41 99t99 41h100v80H260Zm220-280Z"/></svg>
+                    <span>${chrome.i18n.getMessage('newInstallImport')}</span>
+                </button>
+            </div>
         </div>
     `;
+
     fragment.appendChild(noBookmarksDiv);
 
     // Optimize container update using batch insert
